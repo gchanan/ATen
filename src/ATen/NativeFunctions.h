961c24a325aeb68cc720ccb971e91255a559b1ab
Gregory Chanan <gchanan@fb.com>
2017-10-26 08:40:13 -0700

    Implement expand as a native function.
    The logic is the same as in TH/THC, but it operates on the sizes
    as seen by ATen, rather than TH.  This allows us to change the
    sizes representation in ATen independently from TH/THC.

--- src/ATen/NativeFunctions.h
+++ src/ATen/NativeFunctions.h
@@ -2,6 +2,7 @@
 
 #include "ATen/ATen.h"
 #include "ATen/WrapDimUtils.h"
+#include "ATen/ExpandUtils.h"
 #include <vector>
 
 namespace at {
@@ -96,6 +97,30 @@ static inline Tensor permute(const Tensor & self, IntList dims) {
     newStrides[i] = oldStrides[dim];
   }
   return self.as_strided(newSizes, newStrides);
+}
+
+/*
+[NativeFunction]
+name: expand
+arg: Tensor self
+arg: IntList sizes
+return: Tensor
+variants: method, function
+type_method_definition_level: base
+type_method_definition_dispatch: at::native::expand
+[/NativeFunction]
+*/
+static inline Tensor expand(const Tensor &self, IntList sizes) {
+  if (sizes.size() < (size_t)self.dim()) {
+    throw std::runtime_error("the number of sizes provided must be greater or equal to the "
+                             "number of dimensions in the tensor");
+  }
+
+  std::vector<int64_t> expandedSizes;
+  std::vector<int64_t> expandedStrides;
+  std::tie(expandedSizes, expandedStrides) = inferExpandGeometry(self, sizes);
+
+  return self.as_strided(expandedSizes, expandedStrides);
 }
 
 }
